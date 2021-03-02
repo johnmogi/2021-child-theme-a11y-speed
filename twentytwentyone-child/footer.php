90f2ff06b22afff17a6b74e0d99916c2a24e5c11
johnmogi <anguru@gmail.com>
2021-03-02 10:44:31 +0200

    some changes and cleanups

---
+++ twentytwentyone-child/footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+
+	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+<p>ui design by<a href="https://dribbble.com/shots/10744413-Profile-dashboard-XD-freebie">Zhenya Karapetyan</a></p>
+	<p>Photo by&nbsp;<strong><a href="https://www.pexels.com/@jeshoots?utm_content=attributionCopyText&amp;utm_medium=referral&amp;utm_source=pexels">JÉSHOOTS</a></strong>&nbsp;from&nbsp;<strong><a href="https://www.pexels.com/photo/blurred-motion-of-illuminated-railroad-station-in-city-253647/?utm_content=attributionCopyText&amp;utm_medium=referral&amp;utm_source=pexels">Pexels</a></strong></p>
+
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
